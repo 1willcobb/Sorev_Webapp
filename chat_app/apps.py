ab4009361762c8ac862877df33deb180cfb8b66b
1willcobb <cobb.will@gmail.com>
2023-05-11 09:01:37 -0700

    added static files and began building out framework in the main sorev file.

--- chat_app/apps.py
+++ chat_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CreatorChatConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "creator_chat"
+    name = "chat_app"
--- host_app/apps.py
+++ host_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CourseHostConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "course_host"
+    name = "host_app"
